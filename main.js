c625d5e56ef1fb591ac1bf70085a667c349fb690
Narjis Fatima <161502591+NarjisFatima62@users.noreply.github.com>
2024-05-11 21:07:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+//Question Number: 11
+//Names: Store the names of a few of your friends in a array called names.Print each person name by accessing each element in the list, one at a time.
+//let names: string [] = ["Sara","Dua","Asia","Zia"];
+//console.log(names[0]);
+//console.log(names[1]);
+//console.log(names[2]);
+//console.log(names[3]);
+//Question Number: 12
+//Greatings:Start with the array you used in exercise 11, but instead of just printing each person's name,should be the same,but each message be personalize with the person's name.
+let names = ["Sara", "Dua", "Asia", "Zia"];
+let message = "Do you like to play tennis?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
